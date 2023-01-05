87cfd84543398aa0b24563b0f180da3ba95927c3
Theo-Ross <TheoRoss99@icloud.com>
2023-01-05 15:44:45 +0000

    package setup complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+        private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome to Hangman press Enter to play");
+        String startPlay = scanner.nextLine();
+        if (startPlay.contains("Enter")) {
+            System.out.println("Let's Begin!");
+
+        } else {
+            System.out.println("Welcome to Hangman press Enter to play");
+        }
+
+
+
     }
 }
